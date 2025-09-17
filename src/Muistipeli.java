d2ca2802e25c6016a61ae677f17838027270e363
Timi Viisainen <arsyttavakaapa@gmail.com>
2025-09-17 17:21:51 +0300

    Array loopin ja satunnaisen luvun luominen

--- src/Muistipeli.java
+++ src/Muistipeli.java
@@ -1,5 +1,14 @@
+import java.util.Random;
 public class Muistipeli {
     public static void main(String[] args) throws Exception {
+
+        int[] numerot = new int[7];
+        Random r= new Random();
+
+        for (int i=0;i<numerot.length;i++)
+        {
+            numerot[i]=r.nextInt(5)+1;
+        }
 
     }
 }
